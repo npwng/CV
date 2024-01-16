d9bad1a03854bfa7c1a94886cf72d475e4f25b19
npwng <nicholas.probstwong@tutanota.com>
2024-01-16 12:13:43 -0700

    Added global navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,51 @@
+// navbarData.js
+const navbarData = [
+  { label: 'Home', link: 'index.html' },
+  { label: 'About', link: 'about.html' },
+  { label: 'Services', link: '#' },
+  // Add more items as needed
+];
+// navbarGenerator.js
+function generateNavbar(pageContainer) {
+  const navbarContainer = document.createElement('header');
+  navbarContainer.classList.add('navHeader');
+
+  const navline = document.createElement('div');
+  navline.id = 'navi';
+  navline.classList.add('navline');
+
+  // First item to be left aligned
+
+  const link = document.createElement('a');
+  link.href = navbarData[0].link;
+  link.textContent = navbarData[0].label;
+  navline.appendChild(link);
+  navbarData.shift();
+
+  // Main Content
+  const navbarList = document.createElement('nav');
+  navbarList.classList.add('navbar');
+  navbarData.forEach(item => {
+    const link = document.createElement('a');
+    link.href = item.link;
+    link.textContent = item.label;
+    navbarList.appendChild(link);
+  });
+  navline.appendChild(navbarList);
+  navbarContainer.appendChild(navline);
+
+  // Padding
+  const navPadding = document.createElement('div');
+  navPadding.classList.add('navbarPading');
+  navbarContainer.appendChild(navPadding);
+
+  console.log(navbarContainer)
+  pageContainer.parentNode.replaceChild(navbarContainer, pageContainer);
+}
+
+
+
+
 $(document).ready(function () {
     var lastScrollTop = 0;
 
